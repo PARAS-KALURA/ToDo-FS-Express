2ce56e8c4c6c80eb7d2b2e44d855ad13dd53b1ef
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-01-19 02:07:50 +0530

    Day 1 mostly complete

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const { Pool } = require('pg');
 
 const app = express();
 const PORT = 3000;
 
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "todo_app",
+  password: "your_password",
+  port: 5432,
+});
+
+
 // this is middleware- which will help to read json body
 app.use(express.json());
 
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 app.get("/", (req, res) => {
-    res.send("ToDO API is running");
+res.send("ToDO API is running");
 });
 
 app.listen(PORT, () => {
