a2b717df2c71222e3733162ff9e2299b5d40714a
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-01-21 00:23:09 +0530

    middleware

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 // Import and SetUp
 const express = require("express");
-const {Pool} = require("pg");//  bridge bw node.js and my DB
+const {Pool} = require("pg");//  bridge bw node.js and my DB //  Bring me the tool (blueprint) that knows how to talk to PostgreSQL
 
 const app = express();
 const PORT = 3000;
 
-// DB connection
 
-const pool = new Pool({
+// DB connection - This creates a permanent connection manager.
+const pool = new Pool({   // “Use that tool to create a real working connection to my database.
   user: "postgres",
   host: "localhost",
   database: "todo_app",
   password: "Paras@2003",
   port: "5432",
 })
+
+
+// middleware - If someone sends JSON data to my server, read it and make it usable for me.
+app.use(express.json()); 
+
+
