eaa2a62770f07baa099f1c50a0f2fd60c16ad2c8
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-01-21 00:02:44 +0530

    DB connected

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // Import and SetUp
 const express = require("express");
 const {Pool} = require("pg");//  bridge bw node.js and my DB
+
+const app = express();
+const PORT = 3000;
+
+// DB connection
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "todo_app",
+  password: "Paras@2003",
+  port: "5432",
+})
