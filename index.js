5a9751f62c88726b360d1d12a199c9e2cf133b14
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-02-01 14:43:30 +0530

    backend going

--- index.js
+++ index.js
@@ -1,75 +1,15 @@
-const express = require('express');
-//“From the pg package, take out the tool called Pool that knows how to talk to PostgreSQL.”
-const { Pool } = require("pg");
-const cors = require('cors');
+const express = require("express");
+const {Pool} = require("pg");
 
-const app = express();
 const PORT = 3000;
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "todo_app",
-  password: "Paras@2003",
-  port: "5432",
-})
+const app = express();
 
-app.use(express.json());
-app.use(cors());
-
-app.get("/db-test", async (req, res) => {
-  const result = await pool.query("SELECT NOW()");
-  res.json(result.rows[0]);
-} )
-
-app.post("/todos", async (req, res) => {
-  const {title} = req.body;
-  
-  const result = await pool.query("INSERT INTO todos (title) VALUES ($1) RETURNING *",
-    [title]
-  );
-  
-  res.status(201).json(result.rows[0]);
-})
-
-app.delete("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    await pool.query("DELETE FROM todos WHERE id = $1", [id]);
-    
-    res.json({ message: "Todo deleted" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-
-
-// update
-app.put("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { completed } = req.body;
-
-    const result = await pool.query(
-      "UPDATE todos SET completed = $1 WHERE id = $2 RETURNING *",
-      [completed, id]
-    );
-
-    res.json(result.rows[0]);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-
-
-app.get("/todos", async (req, res) => {
-  const result = await pool.query("SELECT * FROM todos ORDER BY id DESC");
-  res.json(result.rows);
+app.get("/", (req,res) => {
+  res.send("Hey there");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server ${PORT} is runnnnnnnnnnning`);
-});
+  console.log(`Server ${PORT} is running`);
+  
+})
