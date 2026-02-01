b7436a4da6e798cc5f17852e338cb8104a0a7254
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-02-01 22:17:01 +0530

    Post ToDo

--- index.js
+++ index.js
@@ -20,17 +20,24 @@ app.get("/", (req,res) => {
 
 
 // create a new todo
+// This is an Express API route that receives todo data from React, saves it in the database, and sends the saved todo back.
 app.post("/", async (req, res) => {
   try {
-   const {description, completed} = req.body; 
-   const newTodo = await pool.query(
-    "INSERT INTO todo (description. completed) VALUES ($1, $2) RETURNING* ",
-    [description, completed]
-  } catch(err) {
+    const { description, completed } = req.body;
+
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description, completed) VALUES ($1, $2) RETURNING *",
+      [description, completed || false]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Server ${PORT} is running`);
