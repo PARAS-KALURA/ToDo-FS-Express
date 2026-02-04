33d21e758d0db80a1558e54770b97dff9c0f3ff8
PARAS-KALURA <112518636+PARAS-KALURA@users.noreply.github.com>
2026-02-05 02:58:50 +0530

    DONE

--- server/models/routes/auth.js
+++ server/models/routes/auth.js
@@ -52,10 +52,28 @@ router.post("/login", async (req, res) => {
 
     try {
 
+      const user = await pool.query(
+        "SELECT * FROM users WHERE email = $1",
+        [email]
+      );
 
+      if(user.rows.length === 0) {
+        return res.status(400).json("Invalid credentails");
+      }
+
+      const validPassword = await bcrypt.compare(
+        password,
+        user.rows[0].password
+      );
+
+      if(!validPassword) {
+        return res.status(400).json("Invalid credentails");
+      }
+
+      res.json("LOGIN SUCCESS");
 
     } catch(err) {
         res.status(500).json("err.message");
     }
 
-} )
+});
